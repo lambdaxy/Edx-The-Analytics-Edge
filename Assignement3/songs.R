0f82b946b059f5de64710d439af195a58b20461f
Frederic Crespo Arredondo <crespo.frederic@gmail.com>
2016-05-06 21:37:57 +0200

    Finished homework 3 section on the songs dataset

--- Assignement3/songs.R
+++ Assignement3/songs.R
@@ -17,3 +17,21 @@ songs_train = songs_train[ , !(names(songs_train) %in% nonvars) ]
 songs_test = songs_test[ , !(names(songs_test) %in% nonvars) ]
 SongsLog1 = glm(Top10 ~ ., data=songs_train, family=binomial)
 summary(SongsLog1)
+
+# Part 3 MultiColinearity
+
+cor(songs$loudness, songs$energy)
+SongsLog2 = glm(Top10 ~ . - loudness, data=songs_train, family=binomial)
+summary(SongsLog2)
+SongsLog3 = glm(Top10 ~ . - energy, data=songs_train, family=binomial)
+summary(SongsLog3)
+
+# Part 4 Model Validation
+
+predictions = predict(SongsLog3, newdata = songs_test, type = "response")
+summary(predictions)
+confusion_matrix = table(songs_test$Top10, predictions > 0.45)
+accuracy = (confusion_matrix[1,1] + confusion_matrix[2,2]) / nrow(songs_test)
+baseline_accuracy = (nrow(songs_test) - sum(songs_test$Top10)) / nrow(songs_test)
+sensitivity = confusion_matrix[2,2] / sum(confusion_matrix[2,])
+specificity = confusion_matrix[1,1] / sum(confusion_matrix[1,])
