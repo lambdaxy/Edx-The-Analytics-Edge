2ffe9603f60cd2efd640bd948ae715834d51a2ea
Frederic Crespo Arredondo <crespo.frederic@gmail.com>
2016-05-08 10:41:33 +0200

    finished parole.R

--- Assignement3/parole.R
+++ Assignement3/parole.R
@@ -22,3 +22,23 @@ specificity = confusion_matrix[1,1] / sum(confusion_matrix[1,])
 baseline_accuracy = (nrow(test) - sum(test$violator)) / nrow(test)
 
 library(ROCR)
+auc = performance(prediction(predictions, test$violator),"auc")
+auc <- as.numeric(auc@y.values)
+
+#onsider a parolee who is male, of white race, aged 50 years at prison release, 
+#from the state of Maryland, served 3 months, had a maximum sentence of 12 months, 
+#did not commit multiple offenses, and committed a larceny
+male = 1
+race = 1
+age = 50
+state2 = 0
+state3 = 0
+state4 = 0
+time.Served = 3
+max.sentence = 12
+multiple.offenses = 0
+crime2 = 1
+crime3 = 0
+crime4 = 0
+odds = exp(model$coefficients[1] + male*model$coefficients['male']+ race*model$coefficients['race']+ age*model$coefficients['age']+ state2*model$coefficients['state2']+ state3*model$coefficients['state3']+ state4*model$coefficients['state4']+ time.Served*model$coefficients['time.served']+ max.sentence*model$coefficients['max.sentence']+ multiple.offenses*model$coefficients['multiple.offenses']+ crime2*model$coefficients['crime2']+ crime3*model$coefficients['crime3']+ crime4*model$coefficients['crime4'])
+prob = odds / (1 + odds)
