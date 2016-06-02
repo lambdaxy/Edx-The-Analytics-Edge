2fade31324df4f4ade9f1b7f65d371564a65c7f0
Frederic Crespo Arredondo <crespo.frederic@gmail.com>
2016-06-02 09:12:20 +0200

    checked if test and train NA's are similar

--- Kaggle/ExploratoryAnalysis.R
+++ Kaggle/ExploratoryAnalysis.R
@@ -3,6 +3,7 @@ library(ggplot2)
 
 # Load Data
 train = read.csv("train2016.csv")
+test = read.csv("test2016.csv")
 
 # Data Cleaning ##################################################################################
 
@@ -12,10 +13,20 @@ train$Gender[train$Gender == ""] = NA
 train$EducationLevel[train$EducationLevel == ""] = NA
 train$HouseholdStatus[train$HouseholdStatus == ""] = NA
 
+test$Income[test$Income == ""] = NA
+test$Gender[test$Gender == ""] = NA
+test$EducationLevel[test$EducationLevel == ""] = NA
+test$HouseholdStatus[test$HouseholdStatus == ""] = NA
+
 sum(is.na(train$Income))/nrow(train)          # 18.5%
 sum(is.na(train$Gender))/nrow(train)          # 2%
 sum(is.na(train$EducationLevel))/nrow(train)  # 15.5%
 sum(is.na(train$HouseholdStatus))/nrow(train) # 8%
+
+sum(is.na(test$Income))/nrow(test)          # 17.6%
+sum(is.na(test$Gender))/nrow(test)          # 2.1%
+sum(is.na(test$EducationLevel))/nrow(test)  # 14.4%
+sum(is.na(test$HouseholdStatus))/nrow(test) # 7.3%
 
 ggplot(data = train, mapping = aes(x = YOB)) + geom_bar()
 ggplot(data = train, mapping = aes(x = Gender)) + geom_bar()
@@ -26,3 +37,7 @@ ggplot(data = train, mapping = aes(x = EducationLevel)) + geom_bar()
 ggplot(data = train, mapping = aes(x = Party)) + geom_bar()
 
 # Lots of young people single with no kids. Data is very much skewed
+# Some clear outlies in the YOB variable
+
+table(train$Income,train$HouseholdStatus)
+
