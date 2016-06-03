4fde9a86d2d2a98160455270bd5d669528b63238
Frederic Crespo Arredondo <crespo.frederic@gmail.com>
2016-06-03 07:04:13 +0200

    worked on kaggle

--- Kaggle/ExploratoryAnalysis.R
+++ Kaggle/ExploratoryAnalysis.R
@@ -1,5 +1,7 @@
 library(mice)
 library(ggplot2)
+library(rpart)
+library(mice)
 
 # Load Data
 train = read.csv("train2016.csv")
@@ -7,16 +9,27 @@ test = read.csv("test2016.csv")
 
 # Data Cleaning ##################################################################################
 
+# Remove YOB outliers
+train = train[train$YOB >= 1900 & train$YOB <= 2003,]
+
 # Replacing empty strings with NA in variables other than questions
 train$Income[train$Income == ""] = NA
+train$Income = factor(train$Income)
 train$Gender[train$Gender == ""] = NA
+train$Gender = factor(train$Gender)
 train$EducationLevel[train$EducationLevel == ""] = NA
+train$EducationLevel = factor(train$EducationLevel)
 train$HouseholdStatus[train$HouseholdStatus == ""] = NA
+train$HouseholdStatus = factor(train$HouseholdStatus)
 
 test$Income[test$Income == ""] = NA
+test$Income = factor(test$Income)
 test$Gender[test$Gender == ""] = NA
+test$Gender = factor(test$Gender)
 test$EducationLevel[test$EducationLevel == ""] = NA
+test$EducationLevel = factor(test$EducationLevel)
 test$HouseholdStatus[test$HouseholdStatus == ""] = NA
+test$HouseholdStatus = factor(test$HouseholdStatus)
 
 sum(is.na(train$Income))/nrow(train)          # 18.5%
 sum(is.na(train$Gender))/nrow(train)          # 2%
@@ -41,3 +54,14 @@ ggplot(data = train, mapping = aes(x = Party)) + geom_bar()
 
 table(train$Income,train$HouseholdStatus)
 
+# Input Income with rpart
+miceMod = mice(train[, !names(train) %in% "Party"], method="rf")
+miceOutput <- complete(incomeMod)
+
+# Test GLM (test again since first way of setting na was not good.)
+SimpleMod = glm(Party ~ . -USER_ID, data=train, family=binomial)
+PredTest = predict(SimpleMod, newdata=test, type="response")
+threshold = 0.5
+PredTestLabels = as.factor(ifelse(PredTest<threshold, "Democrat", "Republican"))
+MySubmission = data.frame(USER_ID = test$USER_ID, PREDICTIONS = PredTestLabels)
+write.csv(MySubmission, "SubmissionSimpleLog.csv", row.names=FALSE)
--- Kaggle/RandomForest.R
+++ Kaggle/RandomForest.R
@@ -9,6 +9,7 @@ train = read.csv("train2016.csv")
 train[is.na(train)] = 1980
 test = read.csv("test2016.csv")
 test[is.na(test)] = 1980
+train = train[train$YOB >= 1900 & train$YOB <= 2003,]
 
 # Prepare Data
 library(caTools)
@@ -25,6 +26,7 @@ confusionMatrix = table(trainTest$Party, predictions)
 accuracy = (confusionMatrix[1,1] + confusionMatrix[2,2]) / nrow(trainTest)
 
 # Submission
+rfModel = randomForest(Party ~ . -USER_ID, data = train, ntree=1000, nodesize = 200)
 PredTest = predict(rfModel, newdata=test)
 MySubmission = data.frame(USER_ID = test$USER_ID, PREDICTIONS = PredTest)
 write.csv(MySubmission, "SubmissionSimpleLogRF.csv", row.names=FALSE)
